bed55ac79f6e55899c849fb3c37eb74b32bfced4
unknown <maggiboy1337@gmail.com>
2022-06-30 11:35:15 +0200

    Add Song to Playlist

--- index.php
+++ index.php
@@ -2,15 +2,18 @@
 
 include_once "./classes/class_playlist.php";
 
+$playlist = new Playlist();
+$playlist->name = "Rock";
+
 
 $octopusSong = new Song();
 $octopusSong->songId = 1;
 $octopusSong->title = "This is my Song";
+$playlist->addSong($octopusSong);
 
-
-$playlist = new Playlist();
-$playlist->name = "Rock";
-$playlist->addSong($octopusSong->title);
-
+$new_song = new Song();
+$new_song->songId = 2;
+$new_song->title = "Another Song";
+$playlist->addSong($new_song);
 
 var_dump($playlist->songs); 
